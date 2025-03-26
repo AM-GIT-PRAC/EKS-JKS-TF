c7ac325d7ed9fdc4e9520f371d74b4b157a4041f
AM-GIT-PRAC <makthala.a@outlook.com>
2025-03-26 01:02:36 -0700

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -10,39 +10,39 @@ module "eks"{
 			most_recent = true
 		}	
 		kube-proxy = {
-		most_recent = true
+			most_recent = true
 		}
 		vpc-cni = {
 			most_recent = true
 		}
+	}
 		
-		vpc_id = module.vpc.vpc_id
-		subnets_ids = module.vpc.private_subnets_tags
-		control_plane_subnets_ids = module.vpc.intra_subnets
+	vpc_id = module.vpc.vpc_id
+	subnet_ids = module.vpc.private_subnets
+	control_plane_subnets_ids = module.vpc.intra_subnets
+
+	eks_managed_node_group_defaults = {
+		ami_type = "AL2_x86_64"
+		instance_type = ["t2.medium"]
+		
+		attach_cluster_primary_security_group = true
+	}
 	
-		eks_managed_node_group_defaults = {
-			ami_type = "AL2_x86_64"
-			instance_type = ["t2.medium"]
+	eks_managed_node_groups = {
+		kk-cluster-wg = {
+			min_size = 1
+			max_size = 2
+			desired_size = 1
 			
-			attach_cluster_primary_security_group = true
-		}
-		
-		eks_managed_node_groups = {
-			kk-cluster-wg = {
-				min_size = 1
-				max_size = 2
-				desired_size = 1
-				
-				instance_types = [t3.medium]
-				capacity_type = "SPOT"
-				
-				tags = {
-					ExtraTag = " HelloWorld "
-				}
+			instance_types = [t3.medium]
+			capacity_type = "SPOT"
+			
+			tags = {
+				ExtraTag = " HelloWorld "
 			}
-		
 		}
-		
-		tags = local.tags
-	}		
-}
+	
+	}
+	
+	tags = local.tags
+}		
